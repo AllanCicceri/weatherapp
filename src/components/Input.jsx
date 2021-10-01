a5c2a5831bd90c087418ef39a9265b4180bdac67
Allan Cicceri <allan.cicceri@gmail.com>
2021-09-30 21:13:49 -0300

    Solved the filter bug. Now it's working properly

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,42 +4,34 @@ import * as BiIcons from 'react-icons/bi'
 import cities from '../Api/cities.json'
 
 export default ({getCityId}) => {
-    const [cityInput, setCityInput] = useState('')
-    const [citiesList, setCitiesList] = useState([])
     const inputElement = useRef(null)
 
-    useEffect(() => {
-      !!citiesList.length && handleCities()
-    }, [citiesList])
-
-
-    const handleCities = () => {  
-      console.log('oi')
+    const handleCities = () => {
+      
       const results = [] 
       let count = 0
-
       Array.from(cities).forEach(item => {
-        if(count < 20)
-          if(item.name.includes(cityInput)){
+        if(count < 10)
+          if(item.name.includes(inputElement.current.value)){
             results.push(item)
             count++
           }
       })
-
-      console.log(results)
-      setCitiesList(results)
-      citiesList!== null && suggestCities()
+      
+      suggestCities(results)
     }
 
 
-    const suggestCities = () => {
+    const suggestCities = (citiesList) => {
+      closeSugestionList()
+      
       const sugestionList = document.createElement('div')
       sugestionList.setAttribute('id', 'sugestionList')
       sugestionList.setAttribute('class', 'autocomplete-items')
 
       inputElement.current.parentNode.appendChild(sugestionList)
       
-      citiesList.forEach((item, i) => {
+      citiesList.forEach((item) => {
         const sugestionItem = document.createElement('div')
         sugestionItem.innerHTML = `${item.name}, ${item.country}`
         sugestionItem.onclick = () =>  {
@@ -54,7 +46,7 @@ export default ({getCityId}) => {
     const closeSugestionList = () => {
       const elements = document.getElementById("searchBar").querySelectorAll(".autocomplete-items")
       
-      elements.forEach((item,i) => {
+      elements.forEach((item) => {
         item.parentNode.removeChild(item)
       })
 
@@ -64,7 +56,7 @@ export default ({getCityId}) => {
         <form autoComplete="off" className="searchContainer">
             <div className="searchBar" id="searchBar">
               <BiIcons.BiSearch size="30px" style={{width:"10%"}}/>
-              <input ref={inputElement} id="inputCitySelector"  value={cityInput} onChange={e => setCityInput(e.target.value)}></input> 
+              <input ref={inputElement} id="inputCitySelector"  onChange={handleCities}></input> 
             </div>
         </form>
     )
